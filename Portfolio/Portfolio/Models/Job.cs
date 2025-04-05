a87133cfd4ae9c3d695b7514293b35a9720358d4
skucik20 <skucik20@gmail.com>
2025-04-05 22:04:11 +0200

    First commit -> shech of a portfolio page

---
+++ Portfolio/Portfolio/Models/Job.cs
@@ -0,0 +1,10 @@
+﻿namespace Portfolio.Models;
+
+public class Job
+{
+    public string Name { get; set; }
+    public string Title { get; set; }
+    public string Time { get; set; }
+    public string Description { get; set; }
+    public string Logo { get; set; }
+}
